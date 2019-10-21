66c1c167bdf6dc6465d48d0565dcbdce76341cca
Phan Hieu Nguyen <fanhieu94@gmail.com>
2019-10-21 17:22:58 -0400

    clean up ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
-import { QuillModule } from 'ngx-quill';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpModule } from '@angular/http';
-// ngrx
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
-import { CurrentConditionsEffects } from './effects/current-conditions.effects';
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
+import { QuillModule } from "ngx-quill";
+import { HttpClientModule } from "@angular/common/http";
+import { HttpModule } from "@angular/http";
 // material
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import {
   MatToolbarModule,
@@ -31,30 +24,21 @@ import {
   MatAutocompleteModule,
   MatDatepickerModule,
   MatNativeDateModule
-} from '@angular/material';
+} from "@angular/material";
 // import {  } from '@angular/material/tabs';
 
-import { AppRoutingModule } from './app-routing.module';
-import { IapContentModule } from './iap/iap-admin/iap-content/iap-content.module';
-import { AppComponent } from './app.component';
-import { ZipcodeEntryComponent } from './zipcode-entry/zipcode-entry.component';
-import { CurrentConditionsComponent } from './current-conditions/current-conditions.component';
-
-import { LocationService } from './location.service';
-import { WeatherService } from './weather.service';
-import { WeatherComponent } from './weather/weather.component';
-import { IapComponent } from './iap/iap.component';
-import { IapAdminComponent } from './iap/iap-admin/iap-admin.component';
-import { HeaderComponent } from './iap/iap-admin/header/header.component';
-import { IapContentComponent } from './iap/iap-admin/iap-content/iap-content.component';
-import { IapHomeComponent } from './iap/iap-admin/iap-home/iap-home.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { IapContentModule } from "./iap/iap-admin/iap-content/iap-content.module";
+import { AppComponent } from "./app.component";
+import { IapComponent } from "./iap/iap.component";
+import { IapAdminComponent } from "./iap/iap-admin/iap-admin.component";
+import { HeaderComponent } from "./iap/iap-admin/header/header.component";
+import { IapContentComponent } from "./iap/iap-admin/iap-content/iap-content.component";
+import { IapHomeComponent } from "./iap/iap-admin/iap-home/iap-home.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ZipcodeEntryComponent,
-    CurrentConditionsComponent,
-    WeatherComponent,
     IapComponent,
     IapAdminComponent,
     HeaderComponent,
@@ -87,20 +71,9 @@ import { IapHomeComponent } from './iap/iap-admin/iap-home/iap-home.component';
     MatAutocompleteModule,
     MatTableModule,
     MatDatepickerModule,
-    MatNativeDateModule,
-    // Ngrx
-    StoreModule.forRoot(reducers, {
-      metaReducers,
-      runtimeChecks: {
-        strictStateImmutability: true,
-        strictActionImmutability: true
-      }
-    }),
-    // EffectsModule.forFeature([CurrentConditionsEffects]),
-    // EffectsModule.forRoot([CurrentConditionsEffects]),
-    !environment.production ? StoreDevtoolsModule.instrument() : []
+    MatNativeDateModule
   ],
-  providers: [LocationService, WeatherService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
