22d10318877027af8d46e1375dd6c6367793d9d1
Tashi Tsering <48037221+TashiTee@users.noreply.github.com>
2019-11-18 14:50:59 -0500

    changes to the content

--- src/app/iap/iap-admin/header/header.component.ts
+++ src/app/iap/iap-admin/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   constructor() {}
 
+  isExpanded = true;
   ngOnInit() {}
 }
--- src/app/iap/iap-admin/iap-content/admin-newsfeeds/admin-newsfeed/admin-newsfeed.component.ts
+++ src/app/iap/iap-admin/iap-content/admin-newsfeeds/admin-newsfeed/admin-newsfeed.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { AdminNewsfeedsService } from '../admin-newsfeeds.service';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import { Location } from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {AdminNewsfeedsService} from '../admin-newsfeeds.service';
+import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-admin-newsfeed',
@@ -19,30 +19,30 @@ export class AdminNewsfeedComponent implements OnInit {
   // editorContent: string;
 
   editorStyle = {
-    height: '300px'
+    height: '200px'
   };
 
-  config = {
-    toolbar: [
-      ['bold', 'italic', 'underline', 'strike'], // toggled buttons
-      ['blockquote', 'code-block'],
+  // config = {
+  //   toolbar: [
+  //     ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+  //     ['blockquote', 'code-block'],
 
-      [{ header: 1 }, { header: 2 }], // custom button values
-      [{ list: 'ordered' }, { list: 'bullet' }],
-      [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
-      [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
-      [{ direction: 'rtl' }], // text direction
+  //     [{ header: 1 }, { header: 2 }], // custom button values
+  //     [{ list: 'ordered' }, { list: 'bullet' }],
+  //     [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+  //     [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+  //     [{ direction: 'rtl' }], // text direction
 
-      [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
-      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  //     [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+  //     [{ header: [1, 2, 3, 4, 5, 6, false] }],
 
-      [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-      [{ font: [] }],
-      [{ align: [] }],
+  //     [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  //     [{ font: [] }],
+  //     [{ align: [] }],
 
-      ['clean']
-    ]
-  };
+  //     ['clean']
+  //   ]
+  // };
 
   constructor(private activeRoute: ActivatedRoute, private newsfeedService: AdminNewsfeedsService, private location: Location) {}
 
