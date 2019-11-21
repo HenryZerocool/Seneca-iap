2ee72cfb0c0d6872bfbc4e596ff6167ddc61ef79
Cecilia <50991907+ysycecilia@users.noreply.github.com>
2019-11-21 09:51:11 -0500

    navbar updates

--- src/app/iap/iap-admin/header/header.component.ts
+++ src/app/iap/iap-admin/header/header.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-
+import { MatSidenav } from '@angular/material';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('sidenav', { static: false }) sidenav: MatSidenav;
   isExpanded = true;
   showSubmenu = false;
   isShowing = false;
-  showSubSubMenu = false;
 
   constructor() {}
 
